f469b42861c6d3a8155b99dbc90f707ad1e6fbcd
Eldiiar Zhyldyzbekov <zhyldyzbekoveldiiar@gmail.com>
2021-09-25 09:53:05 +0600

    create BaseEntity, User and Place

---
+++ src/main/java/kg/amancompany/demo/entity/Place.java
@@ -0,0 +1,14 @@
+package kg.amancompany.demo.entity;
+
+import java.util.List;
+
+public class Place extends BaseEntity {
+
+    private String nameOfPlace;
+
+    private String description;
+
+    private List<String> comment;
+
+    private Double reiting
+}
---
+++ src/main/java/kg/amancompany/demo/entity/User.java
@@ -0,0 +1,24 @@
+package kg.amancompany.demo.entity;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class User extends BaseEntity{
+
+
+    @Size(min=4, max=24, message = "Length must be >= 4 and <= 24")
+    @Pattern(regexp = "^[^\\d\\s]+$", message = "Should contain only letters")
+    private String name;
+
+    @Size(min=4, max=24, message = "Length must be >= 4 and <= 24")
+    @Pattern(regexp = "^[^\\d\\s]+$", message = "Should contain only letters")
+    private String surname;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private String password;
+}
